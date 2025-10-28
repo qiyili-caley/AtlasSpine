2d245eb7c2b3acaf108d3ec2b6e732701b3b6e77
qiyili-caley <qiyili.caley@gmail.com>
2025-10-28 19:37:37 +0800

    Update txt_result_turn_mask.py

--- txt_result_turn_mask.py
+++ txt_result_turn_mask.py
@@ -4,7 +4,7 @@ import os
 import re
 
 def clean_path(path):
-    # 只用于路径！不要用于数据内容
+    # 只用于路径
     return re.sub(r'[\x00-\x1F\x7F]', '', path.strip())
 
 # 文件夹设置
@@ -39,7 +39,7 @@ for label_file in os.listdir(label_dir):
         continue
 
     H, W = img.shape[:2]
-    mask = np.zeros((H, W), dtype=np.uint8)  # 全黑底板
+    mask = np.zeros((H, W), dtype=np.uint8)  
 
     label_path = os.path.join(label_dir, label_file)
     print(f"[INFO] 正在读取标注: {repr(label_path)}")
@@ -63,14 +63,15 @@ for label_file in os.listdir(label_dir):
         cx, cy, w, h, angle = map(float, [cx, cy, w, h, angle])
         # 弧度转角度，OpenCV要求angle为度
         angle_degree = angle * 180 / np.pi
-        # 构造椎骨旋转矩形
+        
         rect = ((cx, cy), (w, h), angle_degree)
         box = cv2.boxPoints(rect)
         box = np.int32(box)
-        # 在mask上填充白色椎骨区域
+        
         cv2.fillPoly(mask, [box], 255)
 
     # 保存mask
     out_path = os.path.join(out_dir, f"{img_name}_mask.png")
     cv2.imwrite(out_path, mask)
+
     print(f"[OK] 已保存mask：{repr(out_path)}")
